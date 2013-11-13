4cdb44401c018a9ec9b30bc9821ed954b640b44a
cchc <spereira@cchc.cl>
2013-11-13 12:00:06 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Solicitudes</title>
+<link href="css/requerimiento.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<center><BR /><BR /><BR />
+<form action="solicitud.php" method="post"  >
+	<table id="fondo1" width="600" height="110"  border="0" >
+	  <tr>
+    	<td>&nbsp;</td>
+	    <td>&nbsp;</td>
+    	<td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	  </tr>
+	  <tr>
+    	<td>&nbsp;</td>
+	    <td align="right">Ingresa tu RUT :</td>
+	    <td align="left">
+	        <p><input name="rut" type="text" value="Ej: 123456789" style="color:#666" onfocus="this.value='';this.style.color='#000000'" maxlength="10" ><font size="2"> Rut sin puntos ni guión.</font></p>
+            
+        </td>
+	    <td>&nbsp;</td>
+	  </tr>
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td align="left"><input type="submit" value="Siguiente" /></td>
+	    <td>&nbsp;</td>
+	  </tr>
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	  </tr>
+	</table>
+</form>
+</center>
+</body>
+</html>
